d62821c416a5edd9b5afe3dc457983726bd81f76
zelef <crazzzy0meat@gmail.com>
2023-02-11 00:00:10 +0600

    2 task complite

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input 1 number: ");
+string firstString = Console.ReadLine();
+int firstNumber = Convert.ToInt32(firstString);
+Console.Write("Input 2 number: ");
+string secondString = Console.ReadLine();
+int secondNumber = Convert.ToInt32(secondString);
+int max = 0;
+if (firstNumber > secondNumber) { max = firstNumber; }
+else  max = secondNumber;
+Console.WriteLine(max);
