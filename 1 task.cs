599531b6c3f2d214395250e109796e4ac2bc5a73
zelef <crazzzy0meat@gmail.com>
2023-02-16 22:04:59 +0600

    done home task 2

--- 1 task.cs
+++ 1 task.cs
@@ -61,71 +61,106 @@
 // else Console.WriteLine(secondDigit);
 
 
-//// Версия с магией
-int randomNumber = Random.Shared.Next(1,99);
-Console.Write("Загадываем число.");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(800);
-Console.WriteLine(" ");
-Console.WriteLine($"Наше число: {randomNumber}!");
-int firstRandomDigit = randomNumber / 10;
-int secondRandomDigit = randomNumber % 10;
-System.Threading.Thread.Sleep(2000);
-Console.WriteLine("*** Производим магию вычислений! ***");
-System.Threading.Thread.Sleep(2000);
-Console.WriteLine($"Наша первая цифра: {firstRandomDigit}");
-System.Threading.Thread.Sleep(300);
-Console.WriteLine($"Наша вторая цифра: {secondRandomDigit}");
-Console.Write("Считаем.");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-Console.WriteLine(" ");
-System.Threading.Thread.Sleep(800);
-if (firstRandomDigit > secondRandomDigit) {Console.WriteLine($"Цифра {firstRandomDigit} больше чем {secondRandomDigit}!");}
-else if (firstRandomDigit < secondRandomDigit) {Console.WriteLine($"Цифра {secondRandomDigit} больше чем {firstRandomDigit}!");}
-else if (firstRandomDigit == secondRandomDigit) {Console.WriteLine($"Вот это повезло {firstRandomDigit} = {secondRandomDigit}");}
-else {Console.WriteLine("Магия не сработала :(");}
+// //// Версия с магией
+// int randomNumber = Random.Shared.Next(1,99);
+// Console.Write("Загадываем число.");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(800);
+// Console.WriteLine(" ");
+// Console.WriteLine($"Наше число: {randomNumber}!");
+// int firstRandomDigit = randomNumber / 10;
+// int secondRandomDigit = randomNumber % 10;
+// System.Threading.Thread.Sleep(2000);
+// Console.WriteLine("*** Производим магию вычислений! ***");
+// System.Threading.Thread.Sleep(2000);
+// Console.WriteLine($"Наша первая цифра: {firstRandomDigit}");
+// System.Threading.Thread.Sleep(300);
+// Console.WriteLine($"Наша вторая цифра: {secondRandomDigit}");
+// Console.Write("Считаем.");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// Console.WriteLine(" ");
+// System.Threading.Thread.Sleep(800);
+// if (firstRandomDigit > secondRandomDigit) {Console.WriteLine($"Цифра {firstRandomDigit} больше чем {secondRandomDigit}!");}
+// else if (firstRandomDigit < secondRandomDigit) {Console.WriteLine($"Цифра {secondRandomDigit} больше чем {firstRandomDigit}!");}
+// else if (firstRandomDigit == secondRandomDigit) {Console.WriteLine($"Вот это повезло {firstRandomDigit} = {secondRandomDigit}");}
+// else {Console.WriteLine("Магия не сработала :(");}
 
-//// хе
-int random = Random.Shared.Next(100,999);
-int firstDigit = random / 100;
-int lastDigit = random % 10;
-int middleDigit = (random / 10) % 10;
-Console.Write("Загадываем число.");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(800);
-Console.WriteLine(" ");
-Console.WriteLine($"Наше число: {random}!");
-System.Threading.Thread.Sleep(1600);
-Console.WriteLine(random + " <- Вот наше число");
-System.Threading.Thread.Sleep(800);
-Console.WriteLine($"{random} <- А теперь магия исчезновения");
-System.Threading.Thread.Sleep(1500);
-Console.WriteLine($"{firstDigit} {lastDigit} <- Хоба!");
-System.Threading.Thread.Sleep(800);
-Console.Write(".");
-System.Threading.Thread.Sleep(300);
-Console.Write(".");
-System.Threading.Thread.Sleep(300);
-Console.Write(".");
-System.Threading.Thread.Sleep(300);
-Console.WriteLine(" ");
-if (middleDigit == 0) {
-    Console.WriteLine($"А куда же делся {middleDigit}?");
-    System.Threading.Thread.Sleep(1200);
-    Console.WriteLine("Его больше никто так и не видел...");
-    }
-else {
-    Console.WriteLine("А куда же делась цифра {middleDigit}?");
-    System.Threading.Thread.Sleep(1200);
-    Console.WriteLine("Её больше никто так и не видел...");
-}
+// //// хе
+// int random = Random.Shared.Next(100,999);
+// int firstDigit = random / 100;
+// int lastDigit = random % 10;
+// int middleDigit = (random / 10) % 10;
+// Console.Write("Загадываем число.");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(800);
+// Console.WriteLine(" ");
+// Console.WriteLine($"Наше число: {random}!");
+// System.Threading.Thread.Sleep(1600);
+// Console.WriteLine(random + " <- Вот наше число");
+// System.Threading.Thread.Sleep(800);
+// Console.WriteLine($"{random} <- А теперь магия исчезновения");
+// System.Threading.Thread.Sleep(1500);
+// Console.WriteLine($"{firstDigit} {lastDigit} <- Хоба!");
+// System.Threading.Thread.Sleep(800);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(300);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(300);
+// Console.Write(".");
+// System.Threading.Thread.Sleep(300);
+// Console.WriteLine(" ");
+// if (middleDigit == 0) {
+//     Console.WriteLine($"А куда же делся {middleDigit}?");
+//     System.Threading.Thread.Sleep(1200);
+//     Console.WriteLine("Его больше никто так и не видел...");
+//     }
+// else {
+//     Console.WriteLine("А куда же делась цифра {middleDigit}?");
+//     System.Threading.Thread.Sleep(1200);
+//     Console.WriteLine("Её больше никто так и не видел...");
+// }
 
+
+
+// Console.Write("Input ur num: ");
+// int inputNumber = Convert.ToInt32(Console.ReadLine());
+// if (inputNumber % 7 == 0 && inputNumber % 23 == 0) {Console.WriteLine("усё намана");}
+// else {Console.WriteLine($"{inputNumber} не кратен 7 и 23");}
+
+
+///////////////////////HOME TASK 2
+///////////////////////HOME TASK 2
+///////////////////////HOME TASK 2
+///////////////////////HOME TASK 2
+
+//Домашняя работа (не использовать для решения задач представления числа в виде строки (т.е. не брать число как массив))
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write(" Input number from 100 to 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int middle = (number / 10) % 10;
+// Console.WriteLine(middle);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (номер цифры считается от левого края)
+Console.Write(" Input number from 100: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) {Console.WriteLine("Error");}
+else {while (number > 999) {number = number / 10;}
+    Console.WriteLine(number % 10);}
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write(" Input day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7) {Console.WriteLine("Yes");}
+else if (day < 6 ) {Console.WriteLine("Go work!");} 
+else {Console.WriteLine("Error");}
