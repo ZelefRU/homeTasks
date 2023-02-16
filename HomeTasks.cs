89118598e1eee614d8e759b3ba836a9ee008fc71
zelef <crazzzy0meat@gmail.com>
2023-02-17 01:38:18 +0600

    remove try

--- HomeTasks.cs
+++ HomeTasks.cs
@@ -239,27 +239,26 @@ void Wait(int waitTime){System.Threading.Thread.Sleep(waitTime);}
 
 
 
-Console.WriteLine("\n\n");
-int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
-for (int i = 0; i < nums.Length; i++) {Console.Write($"{nums[i]} ");}
-Console.WriteLine();
-int temp;
-for (int i = 0; i < nums.Length - 1; i++)
-{
-    for (int j = i + 1; j < nums.Length; j++)
-    {
-        if (nums[i] > nums[j])
-        {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
-    }
-}
-Console.WriteLine("Sorted: ");
-for (int i = 0; i < nums.Length; i++)
-{
-    Console.Write($"{nums[i]} ");
-}
-Console.WriteLine("\n\n");
-
+// Console.WriteLine("\n\n");
+// int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+// for (int i = 0; i < nums.Length; i++) {Console.Write($"{nums[i]} ");}
+// Console.WriteLine();
+// int temp;
+// for (int i = 0; i < nums.Length - 1; i++)
+// {
+//     for (int j = i + 1; j < nums.Length; j++)
+//     {
+//         if (nums[i] > nums[j])
+//         {
+//             temp = nums[i];
+//             nums[i] = nums[j];
+//             nums[j] = temp;
+//         }
+//     }
+// }
+// Console.WriteLine("Sorted: ");
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     Console.Write($"{nums[i]} ");
+// }
+// Console.WriteLine("\n\n");
